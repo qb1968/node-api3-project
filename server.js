9560881095b77ce44f712624a736fa6ba470d67b
qb1968 <you@example.com>
2020-02-13 07:47:32 -0500

    Harder than it looks

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+const usersRouter = require('./users/userRouter.js');
+
 const server = express();
+server.use(express.json());
+server.use("/", logger);
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +13,10 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} Request to ${req.originalUrl}`);
+  next();
+}
 
 module.exports = server;
+ 
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,25 +1,74 @@
+  
 const express = require('express');
 
+const Users = require('./userDb.js');
+const Posts = require('../posts/postDb.js');
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
-});
-
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
-});
 
 router.get('/', (req, res) => {
   // do your magic!
+   Users.get(req.query)
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the posts"
+      });
+    });
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  const id = req.params.id;
+  Users.getById(id)
+  .then(user => {
+    if(user) {
+      res.status(200).json(user)
+    } else {
+      res.status(404).json({ message: 'User not found'});
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({message: "Error retrieving this user"})
+  })
 });
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+});
+
+router.post('/', (req, res) => {
+  //do your magic!
+  Users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error adding the post"
+      });
+    }); 
+});
+
+router.post('/:id/posts', (req, res) => {
+  // do your magic!
+  // const id = req.params.id;
+  // const post = { ...req.body, user_id: id };
+  // Users.insert(post)
+  // .then(post => {
+  //   res.status(200).json(post);
+  // })
+  // .catch( error => {
+  //   console.log(error);
+  //   res.status(500).json({message: "Sorry, we can't proceed"});
+  // })
 });
 
 router.delete('/:id', (req, res) => {
